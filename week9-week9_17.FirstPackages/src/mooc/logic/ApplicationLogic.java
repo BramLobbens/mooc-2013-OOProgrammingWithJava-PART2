ba76373476e197274cd29ddf41facb0e4c50bba2
BramLobbens <lobbensbram@gmail.com>
2019-05-31 19:53:25 +0200

    add: new exercise week 9

---
+++ week9-week9_17.FirstPackages/src/mooc/logic/ApplicationLogic.java
@@ -0,0 +1,20 @@
+package mooc.logic;
+
+import mooc.ui.UserInterface;
+
+public class ApplicationLogic {
+
+    private UserInterface ui;
+    
+    public ApplicationLogic(UserInterface ui) {
+        this.ui = ui;
+    }
+    
+    public void execute(int howManyTimes) {
+        while (howManyTimes-- > 0) {
+            System.out.println("The application logic works");
+            ui.update();
+        }
+    }
+
+}
